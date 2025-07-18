9d65eb93719ec743e643d51d1ac3a4c8cb5ecff8
Jesus Bejar <israelbejarii@gmail.com>
2025-07-18 16:57:11 -0600

    first commit

---
+++ src/main/java/com/scripturebookapp/ScriptureBookApp/ScriptureBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.scripturebookapp.ScriptureBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptureBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptureBookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scripturebookapp/ScriptureBookApp/ScriptureBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scripturebookapp.ScriptureBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptureBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
