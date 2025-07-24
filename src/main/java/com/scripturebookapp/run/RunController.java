2837fc7b0d35ba4ca9fe030495d08502b6016ba3
Jesus Bejar <israelbejarii@gmail.com>
2025-07-24 14:36:19 -0600

    made run controller

---
+++ src/main/java/com/scripturebookapp/run/RunController.java
@@ -0,0 +1,12 @@
+package com.scripturebookapp.run;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RunController {
+    @GetMapping("/hello")
+    String home() {
+        return "Welcome to the Run Controller!";
+    }
+}
