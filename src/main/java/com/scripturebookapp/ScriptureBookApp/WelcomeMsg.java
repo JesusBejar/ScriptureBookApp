585a9fa1d934577bdc76ef2ff0cb86e69465d284
Jesus Bejar <israelbejarii@gmail.com>
2025-07-24 15:20:11 -0600

    added hello endpoint

--- src/main/java/com/scripturebookapp/ScriptureBookApp/WelcomeMsg.java
+++ src/main/java/com/scripturebookapp/ScriptureBookApp/WelcomeMsg.java
@@ -1,7 +1,12 @@
 package com.scripturebookapp.ScriptureBookApp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeMsg {
 
+    @GetMapping("/hello")
     public String getWelcomeMsg() {
         return "Welcome msg baby!";
     }
