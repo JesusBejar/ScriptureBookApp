6989c4379fa2d0af25e0c441e1fc12a7027c7b17
Jesus Bejar <israelbejarii@gmail.com>
2025-07-24 15:20:31 -0600

    added run endpoint

--- src/main/java/com/scripturebookapp/run/RunController.java
+++ src/main/java/com/scripturebookapp/run/RunController.java
@@ -1,12 +1,31 @@
 package com.scripturebookapp.run;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// define the base path
+@RequestMapping("/api") 
 public class RunController {
-    @GetMapping("/hello")
-    String home() {
-        return "Welcome to the Run Controller!";
+
+    private static final Logger log = LoggerFactory.getLogger(RunController.class);
+    private final RunRepo runRepo;
+
+    // constructor
+    public RunController(RunRepo runRepo) {
+        this.runRepo = runRepo;
+        log.info("RunController initialized");
+    }
+
+    @GetMapping("/runs")
+    // function to return array of runs
+    List<Run> findAll() {
+        log.info("GET /api/runs endpoint called");
+        return runRepo.findAll();
     }
 }
